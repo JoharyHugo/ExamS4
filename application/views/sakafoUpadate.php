ff5d6a9ede1a9ec7b59371514e0d844575bbfb0b
JoharyHugo <joharyny@gmail.com>
2023-07-11 10:45:32 +0100

    Update Sakafo

---
+++ application/views/sakafoUpadate.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Update</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/vendor/bootstrap-4.1/bootstrap.min.css")?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css")?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/vendors/animate/animate.css")?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/vendor/css-hamburgers/hamburgers.min.css")?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/vendor/select2/select2.min.css")?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/updatesakafo/css/util.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/updatesakafo/css/main.css")?>">
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<div class="contact1">
+		<div class="container-contact1">
+			<div class="contact1-pic js-tilt" data-tilt>
+				<img src="<?php echo base_url("assets/img/sakafo.jpg"); ?>" alt="IMG">
+			</div>
+
+			<form class="contact1-form validate-form" enctype="multipart/form-data">
+				<span class="contact1-form-title">
+					Update Sakafo
+				</span>
+
+				<div class="wrap-input1 validate-input" data-validate = "Name is required">
+					<input class="input1" type="text" name="name" placeholder="Nom Sakafo">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<input class="input1" type="file" name="email" placeholder="Votre photo">
+					<span class="shadow-input1"></span>
+				</div>
+
+				
+
+				<div class="container-contact1-form-btn">
+					<button class="contact1-form-btn">
+						<span>
+							Modifier
+							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+
+
+<!--===============================================================================================-->
+	<script src="<?php echo base_url("assets/vendors/jquery/jquery-3.2.1.min.js");?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url("assets/vendors/bootstrap/js/popper.js");?>"></script>
+	<script src="<?php echo base_url("assets/vendors/bootstrap/js/bootstrap.min.js");?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url("assets/vendor/select2/select2.min.js");?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url("assets/vendor/tilt/tilt.jquery.min.js");?>"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+<!--===============================================================================================-->
+	<script src="<?php echo base_url("assets/updatesakafo/js/main.js"); ?>"></script>
+
+</body>
+</html>
--- application/views/sakafodetail.php
+++ application/views/sakafodetail.php
@@ -1,8 +1,97 @@
+<style>
+    button{
+        background-color:#00e950;
+        color: white;
+        width: 178px;
+        height: 49px;
+        float: right;
+        margin-top: -62px;
+    }
+</style>
 <div class="page-container">
     <div class="main-content">
                 <div class="section__content section__content--p30">
                     <div class="container-fluid">
                         
+                    <div class="row">
+                           
+                            <div class="col-lg-12">
+                                <!-- TOP CAMPAIGN-->
+                                <div class="top-campaign">
+                                    <h3 class="title-3 m-b-30">CRUD Regime</h3>
+                                    <button  > + Creation New Regime </button>
+                                    <div class="table-responsive">
+                                        <table class="table table-top-campaign">
+                                            <tbody>
+                                                <tr>
+                                                    <td>1. Australia</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>2. United Kingdom</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>3. Turkey</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>4. Germany</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>5. France</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>3. Turkey</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>4. Germany</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>5. France</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>3. Turkey</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>4. Germany</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>5. France</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>4. Germany</td>
+                                                    <td><a href="#" style="color: blue;">Modifier</a></td>
+                                                    <td><a href="#" style="color: red;" >Delete</a></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <!--  END TOP CAMPAIGN-->
+                            </div>
+                        </div>
+
+
                     <div class="row">
                             <div class="col-md-12">
                                 <!-- DATA TABLE -->
@@ -50,6 +139,7 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <!-- DATA TABLE -->
+                                <br>
                                 <h3 class="title-5 m-b-35">CRUD Sport</h3>
                               
                                 <div class="table-responsive table-responsive-data2">
